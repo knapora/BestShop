0e03905ddc99186f1d0eeec987d580f89646640b
knapora <krzysztof.napora@op.pl>
2024-07-05 10:22:00 +0200

    added reset/css/html basic files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 /**
  * Path to exercise folder
  */
-const exercisePath = "01_Car_Sharing";
+const exercisePath = "BestShop";
 
 /**
  * Don't change those lines below
